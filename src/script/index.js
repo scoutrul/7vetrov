bc921252d2658c73391a4034057a92165075ad21
scoutrul <scoutrul@mail.ru>
2018-01-30 23:20:11 +0300

    sidebar menu toggler js

---
+++ src/script/index.js
@@ -0,0 +1,7 @@
+
+//sidebar menu toggler
+document.querySelectorAll('.sidebar__menu')
+	.forEach(e => {
+		e.getElementsByTagName('a')[0].addEventListener('click', () => e.classList.toggle('sidebar__menu-active'))
+	});
+
